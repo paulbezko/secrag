a25978307980c0dcbad941793a90745ca4632afd
Paul Manart <112270062+Paul-Manart@users.noreply.github.com>
2024-10-15 19:59:09 +0200

    frontend backend bugfixes

--- client/src/config.js
+++ client/src/config.js
@@ -4,14 +4,14 @@ const dev = {
   webUrl: 'http://localhost:8080'
 };
 
+// const prod = {
+//   apiUrl: 'http://localhost:5000',
+//   webUrl: 'http://localhost:5000'
+// };
+
 const prod = {
   apiUrl: 'https://secrag.com',
   webUrl: 'https://secrag.com'
 };
-
-// const prod = {
-//   apiUrl: 'https://collab-skeltal.onrender.com',
-//   webUrl: 'https://collab-skeltal.onrender.com'
-// };
 
 export const config = process.env.NODE_ENV === 'production' ? prod : dev;
