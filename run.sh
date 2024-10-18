74841761cb2c5e7b64ff754aa3faffee171d90e1
Ubuntu <ubuntu@ip-172-31-38-184.eu-north-1.compute.internal>
2024-10-18 12:00:58 +0000

    improved run script

--- run.sh
+++ run.sh
@@ -5,5 +5,8 @@
 # Navigate to the application directory (modify this to your app's path if needed)
 cd /home/ubuntu/secrag/Collab-Project-1
 
+# Activate the environment
+source ./venv/bin/activate
+
 # Run Gunicorn with the desired settings
 gunicorn --worker-class eventlet -w 1 --bind 0.0.0.0:8000 wsgi:app -c config.py
