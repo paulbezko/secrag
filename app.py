8c1bd79852bed127d0b6de48d70d7ddc30b305af
QuestionMarkSounds <123009724+QuestionMarkSounds@users.noreply.github.com>
2024-10-20 03:14:23 +0300

    get socketio object for debug socketio server

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from server import create_app, handler
 import logging
 
 mode = 'prod' # Controls whether the server will use built client static files or not (prod or dev)
-app = create_app(mode)
+app, socketio = create_app(mode)
 app.logger.setLevel(logging.DEBUG)
 app.logger.addHandler(handler)
 app.logger.addHandler(LogtailHandler(source_token='r7bKwtvkMf9iBBqAsYXmJyFS'))
 
 if __name__ == '__main__':
-    app.run(debug = False)
+    socketio.run(app, debug = False, port = 5000)
