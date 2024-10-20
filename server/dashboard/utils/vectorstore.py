574f0e6f0c63d3e03c8a68914afb4ee6b0a070a3
Paul Manart <112270062+Paul-Manart@users.noreply.github.com>
2024-10-20 21:08:19 +0200

    merge conflict fix

--- server/dashboard/utils/vectorstore.py
+++ server/dashboard/utils/vectorstore.py
@@ -1,78 +1,90 @@
 from langchain_community.vectorstores import FAISS
+from langchain_core.documents import Document
 from langchain_openai import OpenAIEmbeddings
 from ...general.utils import log
 from .secedgar import FilingObject, get_sec_filing_object
+from celery import Celery
 
 import os
 
 
-# Getting vectorstore
-def get_vectorstore(
-        filing : FilingObject, 
-        new_chat : bool,
+celery_app = Celery('tasks', broker='redis://localhost:6379/0', backend=None)
 
+class VectorstoreManager:
+    
+    vectorstore_dir = "database/vectorstore"
+    def __init__(self) -> FAISS:
+        self.vectorstore = self.get_vectorstore()
+
+    # Getting vectorstore
+    def get_vectorstore(
+            self
+        ):
+        """
+        Manages vectorstore for a given filing and configuration.
+        Args:
+            filing (CustomCompanyFiling): The filing object to be converted.
+            chunk_size (int, optional): The maximum size of each chunk. Defaults to 5000.
+            chunk_overlap (int, optional): The number of rows to overlap between chunks. Defaults to 1000.
+            k (int, optional): The number of similar documents to return. Defaults to 1.
+            table_prepend_k (int, optional): The number of rows to prepend to each table chunk. Defaults to 3.
+            splitter_mode (str, optional): The text splitter to use. Defaults to "edgartools".
+        Returns:
+            FAISS: The vectorstore object.
+        """
+        embeddings = OpenAIEmbeddings()
+        # Check if vectorstore exists
+        if os.path.exists(self.vectorstore_dir):
+
+            # Load vectorstore
+            vectorstore = FAISS.load_local(self.vectorstore_dir, embeddings=embeddings, allow_dangerous_deserialization=True)
+            
+        # Create vectorstore if it doesn't exist
+        else:
+            # Create new embedding and vectorstore, and save the vectorstore 
+            init_document_list = [Document(page_content="", metadata={"chunk_description":"init"})]
+            vectorstore = FAISS.from_documents(init_document_list, embedding=embeddings)
+            vectorstore.save_local(self.vectorstore_dir)
+            # log('info', f"Created Vectorstore")
+        return vectorstore
+    
+    def new_chat(
+        self,
+        filing : FilingObject, 
         # Optional args
         chunk_size = 10000, 
         chunk_overlap = 3, 
-        table_prepend_k = 3, 
-    ):
-    """
-    Manages vectorstore for a given filing and configuration.
+        table_prepend_k = 3,
+        ):
 
-    Args:
-        filing (CustomCompanyFiling): The filing object to be converted.
-        chunk_size (int, optional): The maximum size of each chunk. Defaults to 5000.
-        chunk_overlap (int, optional): The number of rows to overlap between chunks. Defaults to 1000.
-        k (int, optional): The number of similar documents to return. Defaults to 1.
-        table_prepend_k (int, optional): The number of rows to prepend to each table chunk. Defaults to 3.
-        splitter_mode (str, optional): The text splitter to use. Defaults to "edgartools".
+        chunk_metadata_model = {
+            "ticker": filing.ticker, 
+            "date": filing.filing_date,
+            "form": filing.filing_type,
+            "year": filing.filing_year,
+            "chunk_description": "",
+            "chunk_size": chunk_size,
+            "chunk_overlap": chunk_overlap,
+            "table_prepend_k": table_prepend_k
+        }
 
-    Returns:
-        FAISS: The vectorstore object.
-    """
+        check_for_existing_embeddings = self.vectorstore.similarity_search("", k=3, filter=chunk_metadata_model)
 
-    # Initialize metadata model
-    chunk_metadata_model = {
-        "ticker": filing.ticker, 
-        "date": filing.filing_date,
-        "form": filing.filing_type,
-        "year": filing.filing_year,
-        "chunk_description": "",
-        "chunk_size": chunk_size,
-        "chunk_overlap": chunk_overlap,
-        "table_prepend_k": table_prepend_k
-    }
+        # Case when embedding does not exist
+        if len(check_for_existing_embeddings) == 0:
+            self._perform_embedding(filing.to_dict(), chunk_size, chunk_overlap, table_prepend_k)
+            log('debug', f"Updated Vectorstore for {filing.ticker}-{filing.filing_date}, {chunk_size}, {chunk_overlap}, {table_prepend_k}")
 
-    vectorstore_dir = "database/vectorstore"
-    embeddings = OpenAIEmbeddings()
+        # Case when embedding already exists       
+        else:
+            log('debug', f"Embedding already exists for {filing.ticker}-{filing.filing_date}, {chunk_size}, {chunk_overlap}, {table_prepend_k}")
 
-    # Check if vectorstore exists
-    if os.path.exists(vectorstore_dir):
 
-        # Load vectorstore
-        vectorstore = FAISS.load_local(vectorstore_dir, embeddings=embeddings, allow_dangerous_deserialization=True)
-
-        # Check if embedding already exists for a given filing and embedding config
-        if new_chat:
-            check_for_existing_embeddings = vectorstore.similarity_search("", k=3, filter=chunk_metadata_model)
-            # Case when embedding does not exist
-            if len(check_for_existing_embeddings) == 0:
-                sec_filing_object = get_sec_filing_object(filing)
-                chunks = sec_filing_object.get_documents(chunk_size, chunk_overlap, table_prepend_k)
-                vectorstore.add_documents(chunks)
-                vectorstore.save_local(vectorstore_dir)
-                log('debug', f"Updated Vectorstore for {filing.ticker}-{filing.filing_date}, {chunk_size}, {chunk_overlap}, {table_prepend_k}")
-            # Case when embedding already exists       
-            else:
-                log('debug', f"Embedding already exists for {filing.ticker}-{filing.filing_date}, {chunk_size}, {chunk_overlap}, {table_prepend_k}")
-
-    # Create vectorstore if it doesn't exist
-    else:
-        # Create new embedding and vectorstore, and save the vectorstore 
+    def _perform_embedding(self, filing_data, chunk_size, chunk_overlap, table_prepend_k):
+        filing = FilingObject.from_dict(filing_data)
         sec_filing_object = get_sec_filing_object(filing)
         chunks = sec_filing_object.get_documents(chunk_size, chunk_overlap, table_prepend_k)
-        vectorstore = FAISS.from_documents(chunks, embedding=embeddings)
-        vectorstore.save_local(vectorstore_dir)
-        log('info', f"Created Vectorstore for {filing.ticker}-{filing.filing_date}")
+        self.vectorstore.add_documents(chunks)
+        self.vectorstore.save_local(self.vectorstore_dir)
 
-    return vectorstore
+vectorstore_manager = VectorstoreManager()
