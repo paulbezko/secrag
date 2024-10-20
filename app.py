b93967212995aa0a9f6b56004650748c7629f1a2
Paul Manart <112270062+Paul-Manart@users.noreply.github.com>
2024-10-20 21:09:07 +0200

    merge conflict fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from logtail import LogtailHandler
 from server import create_app, handler
 import logging
 
-mode = 'dev' # Controls whether the server will use built client static files or not (prod or dev)
+mode = 'prod' # Controls whether the server will use built client static files or not (prod or dev)
 app, socketio = create_app(mode)
 app.logger.setLevel(logging.DEBUG)
 app.logger.addHandler(handler)
