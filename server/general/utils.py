97f0875b850ce27f4afce9724e599005629e9e2c
QuestionMarkSounds <123009724+QuestionMarkSounds@users.noreply.github.com>
2024-11-04 20:57:25 +0200

    Individual postgres connections instead of global connection

--- server/general/utils.py
+++ server/general/utils.py
@@ -266,7 +266,15 @@ def get_user_data(email, retries=3):
     attempt = 0
     while attempt < retries:
         try:
-            connection = current_app.config['DB_CONNECTION']
+            
+            connection = psycopg2.connect(
+                host        = os.getenv('DB_HOST'),
+                port        = os.getenv('DB_PORT'),
+                database    = os.getenv('DB_NAME'),
+                user        = os.getenv('DB_USER'),
+                password    = os.getenv('DB_PASS'),
+                cursor_factory = RealDictCursor
+            )
             with connection.cursor() as cursor:
                 query = f"SELECT * FROM users_{current_app.config['MODE']} WHERE email = %s"
                 cursor.execute(query, (email,))
@@ -275,12 +283,14 @@ def get_user_data(email, retries=3):
         except (OperationalError, InterfaceError) as conn_error:
             log('warning', f'Connection error [Attempt {attempt + 1}/{retries}]: {conn_error}')
             attempt += 1
-            reconnect_to_db()
             time.sleep(1)
         
         except Exception as error:
             log('error', f'Error [Get User Data]: {error}')
             break
+    
+        finally:
+            connection.close()
     
     log('critical', 'Failed to retrieve user data after multiple attempts.')
     return None
@@ -290,7 +300,14 @@ def execute_query(query, params, retries=3):
     attempt = 0
     while attempt < retries:
         try:
-            connection = current_app.config['DB_CONNECTION']
+            connection = psycopg2.connect(
+                host        = os.getenv('DB_HOST'),
+                port        = os.getenv('DB_PORT'),
+                database    = os.getenv('DB_NAME'),
+                user        = os.getenv('DB_USER'),
+                password    = os.getenv('DB_PASS'),
+                cursor_factory = RealDictCursor
+            )
             with connection.cursor() as cursor:
                 cursor.execute(query, params)
                 connection.commit()
@@ -299,30 +316,17 @@ def execute_query(query, params, retries=3):
         except (OperationalError, InterfaceError) as conn_error:
             log('warning', f'Connection error [Attempt {attempt + 1}/{retries}]: {conn_error}')
             attempt += 1
-            reconnect_to_db()
             time.sleep(2)  # Optional delay before retrying
         
         except Exception as error:
             log('error', f'Error [Execute Query]: {error}')
             break  # Exit loop on unexpected exceptions
 
+        finally:
+            connection.close()
+
     log('critical', 'Failed to execute query after multiple attempts.')
 
-
-def reconnect_to_db():
-    try:
-        connection = psycopg2.connect(
-            host=os.getenv('DB_HOST'),
-            port=os.getenv('DB_PORT'),
-            database=os.getenv('DB_NAME'),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASS'),
-            cursor_factory=RealDictCursor
-        )
-        current_app.config['DB_CONNECTION'] = connection
-        log('debug', 'Reconnected to database successfully.')
-    except OperationalError as error:
-        log('error', f'Error [Reconnect to Database]: {error}')
 
 
 def check_timestamp(timestamp):
