151589912598cb5d2da0b94fef8d4de96aa51e34
Paul Manart <112270062+Paul-Manart@users.noreply.github.com>
2024-11-10 18:05:09 +0100

    dashboard textarea getting larger during response fix

--- client/src/views/PreviewView.vue
+++ client/src/views/PreviewView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="windowLoaded" class="flex-column width-100 center gap-1 padding-sidebar-dashboard height-100" style="height: 100svh; padding-bottom: 1rem; max-width: 140rem; overflow: hidden;">
+  <div v-if="windowLoaded" class="flex-column width-100 center gap-1 padding-sidebar-dashboard height-100" style="height: 100dvh; padding-bottom: 1rem; max-width: 140rem; overflow: hidden;">
   
     <!-- Component Section -->
     <div v-if="showProfile" class="backdrop z-17" @click="toggleProfile"></div>
@@ -114,7 +114,7 @@
     </div>
 
     <!-- Chat Section -->
-    <div class="flex-row width-100 gap-1 height-100" style="justify-content: center; max-height: calc(100svh - 4rem);" :style="isSmallScreen ? '' : 'padding: 1rem 1rem 0rem 1rem;'" v-if="!newChat">
+    <div class="flex-row width-100 gap-1 height-100" style="justify-content: center; max-height: calc(100dvh - 4rem);" :style="isSmallScreen ? '' : 'padding: 1rem 1rem 0rem 1rem;'" v-if="!newChat">
       <div class="flex-column width-100 gap-1 center" style="max-width: 75rem; background-color: transparent;">
         <div v-if="!filingShown || !isSmallScreen" class="chat-container text-inter height-100" id="chat-container">
           <SpinnerCompInside v-if="chatLoading" :customClass="'text-3'"></SpinnerCompInside>
