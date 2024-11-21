e0015684fb3cdc0315b9a8d14fb5935e2d7e8c09
QuestionMarkSounds <123009724+QuestionMarkSounds@users.noreply.github.com>
2024-11-21 10:30:07 +0200

    Backup vectorstore on server launch

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@
 # Navigate to the application directory (modify this to your app's path if needed)
 cd /home/ubuntu/secrag/Collab-Project-1
 
+# Make a backup of the vectorstore on startup
+sudo mkdir -p database/vectorstore_backup && sudo cp -rf database/vectorstore/* database/vectorstore_backup/
+
 # Activate the environment
 source ./venv/bin/activate
 
