8781f4be7949d90bb729c20182cb3c4634095510
QuestionMarkSounds <123009724+QuestionMarkSounds@users.noreply.github.com>
2024-11-22 19:38:39 +0200

    More async for Edgartools

--- server/lib_secrag/edgar/xbrl/__init__.py
+++ server/lib_secrag/edgar/xbrl/__init__.py
@@ -11,7 +11,7 @@ from edgar.xbrl.presentation import XBRLPresentation
 from typing import Optional, Union
 
 
-def get_xbrl_object(filing: Filing) -> Optional[Union[XBRLInstance, XBRLData]]:
+async def get_xbrl_object(filing: Filing) -> Optional[Union[XBRLInstance, XBRLData]]:
     """
     Extract XBRL data from a filing
     :param filing: Filing
@@ -23,7 +23,7 @@ def get_xbrl_object(filing: Filing) -> Optional[Union[XBRLInstance, XBRLData]]:
     elif xbrl_documents.instance_only:
         return xbrl_documents.get_xbrl_instance()
     else:
-        return XBRLData.extract(filing)
+        return await XBRLData.extract(filing)
 
 
 
