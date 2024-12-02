96b3b1dbf8a796beebc8fd21674359144e18bdcb
Paul Manart <112270062+Paul-Manart@users.noreply.github.com>
2024-12-02 18:15:36 +0100

    samsung textarea suggestions fix

--- client/src/views/PreviewView.vue
+++ client/src/views/PreviewView.vue
@@ -95,6 +95,7 @@
           <input 
             type="text"
             class="input width-100"
+            v-model="optionInput"
             v-on:input="optionInput = $event.target.value"
             @input="filterOptions" 
             @focus="showSuggestions = true" 
