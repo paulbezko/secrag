1bc5340fa8d4bd36551b1849a33eccf041acb345
Paul Manart <112270062+Paul-Manart@users.noreply.github.com>
2025-01-06 23:16:42 +0100

    removed ass

--- ass.py
+++
@@ -1,8 +0,0 @@
-def save_file_contents(fpath, content, encoding, bom=None):
-    with open(fpath, mode='w', encoding=encoding) as fout:
-        if bom:
-            fout.write(u'\ufeff')
-        fout.write(content)
-        print('Processed', fpath)
-
-save_file_contents('requirements.txt', content, 'utf-16-LE', bom=True)
