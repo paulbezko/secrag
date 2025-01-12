8bf778e019e8ce26b414bf60b0340df004c6350d
QuestionMarkSounds <123009724+QuestionMarkSounds@users.noreply.github.com>
2025-01-12 16:00:01 +0200

    Added Presenter agent. Plotter outputs a structured output instead of toolcalls

---
+++ server/core/dashboard/multiagent/agent_presenter.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from typing import Literal
+from custom_langgraph_methods import create_react_agent_with_node_name
+from globals import State, llm
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langgraph.types import Command
+from langchain_core.messages import AIMessage
+from helpers import get_last_message, get_last_node_message
+
+presenter_system_prompt = """
+        You are given the latest user message and the messages from presenter, archivist, plotter AI agents.
+        Your task is to present the given AI agents' responses to the user. Specifically, you should focus on presenting the plot data if available. If the plot data is present, prioritize it and make a placeholder for the plot [<widget_id>] where <widget_id> is retrieved from plotter's output's widget_id. 
+        Only include relevant text if necessary to accompany the plot or provide context or overview.
+
+        user_message: {user_message}
+
+        concierge_response: {concierge_response}
+
+        archivist_response: {archivist_response}
+
+        plotter_response: {plotter_response} 
+    """
+
+presenter_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", presenter_system_prompt),
+            ]
+)
+
+presenter_chain = presenter_prompt | llm
+
+async def presenter_node(state: State) -> Command[Literal["__end__"]]:
+
+    last_user_message = get_last_message(state["messages"], "user")
+    last_concierge_message = get_last_node_message(state["messages"], "concierge")
+    last_archivist_message = get_last_node_message(state["messages"], "archivist")
+    last_plotter_message = get_last_node_message(state["messages"], "plotter")
+
+    result = await presenter_chain.ainvoke({
+        "user_message": last_user_message.content,
+        "concierge_response": last_concierge_message.content,
+        "archivist_response": last_archivist_message.content,
+        "plotter_response": last_plotter_message.content
+    })
+    
+    return Command(
+        graph="presenter",
+        update={
+            "messages": [
+                AIMessage(
+                    content=result.content, name="presenter"
+                )
+            ]
+        },
+        goto="__end__"
+    )
---
+++ server/core/dashboard/multiagent/plotter_models.py
@@ -0,0 +1,59 @@
+import traceback 
+
+from pydantic import BaseModel, Field
+from typing import Annotated, Literal, List, Optional, Type, Union
+
+from langchain_core.tools import tool
+
+# Time series plot models
+class TimeSeriesTimeDataPoint(BaseModel):
+    year: int
+    month: int
+    day: int
+
+class TimeSeriesDataPoint(BaseModel):
+    time: TimeSeriesTimeDataPoint = Field("The time of the data point")
+    value: float
+
+class TimeSeries(BaseModel):
+    name: str = Field("The name of the series (provide data units (e.g. $ million) in brackets )")
+    data: List[TimeSeriesDataPoint]
+
+class TimeSeriesPlotData(BaseModel):
+    series: List[TimeSeries]
+
+# Treemap plot models
+class TreeMapDataPoint(BaseModel):
+    x: str = Field("The label for the data point")
+    y: float
+
+class TreeMapSeries(BaseModel):
+    name: str = Field("The name of the series (provide data units (e.g. $ million) in brackets )")
+    data: List[TreeMapDataPoint]
+
+class TreemapPlotData(BaseModel):
+    series: List[TreeMapSeries]
+
+# Sankey chart models
+class Node(BaseModel):
+    id: str = Field("Node ID")
+    title: str = Field("human-readable name or title of the node")
+
+class Edge(BaseModel):
+    source: str = Field("the ID of the source node")
+    target: str = Field("the ID of the target node")
+    value: float = Field("the weight of the edge (e.g., the strength of the relationship)")
+
+class SankeyChart(BaseModel):
+    nodes: List[Node] = Field("list of Node objects, each representing a node in the graph")
+    edges: List[Edge] = Field("list of Edge objects, each representing an edge in the graph")
+
+# Master model
+class PlotterOutputModel(BaseModel):
+    widget_id: str
+    plot_type: Literal["time_series", "treemap", "None"]
+    plot_data: Optional[Union[
+        TimeSeriesPlotData,TreemapPlotData, Literal["None"]
+        ]] = Field("Selected plot type data or None if not plotting")
+    reason: str
+
